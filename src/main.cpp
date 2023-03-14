dd2309396bb8dfe8e9822414db6d3563fca1b330
HookSandeer <hooksander@proton.me>
2023-03-14 18:39:12 +0100

    Update script

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <vector>
-#include <cstdlib>
+#include <random>
 
 using namespace std;
 
@@ -10,34 +10,23 @@ void print(string text){
     cout << text << endl;
 }
 
-string sInput(string text){
-    string input;
-    cout << text;
-    getline(cin, input);
-    return input;
-}
-
-int iInput(string text){
-    int input;
-    cout << text;
-    cin >> input;
-    return input;
-}
-
-void clean(){
-    for(int i=0; i<50; i++){
+void clear(){
+    for(int i=0; i<30; i++){
         print("\n");
     }
 }
 
-int randomNumber(){
-    int random = rand()%6;
+int randomNumber(const int sizeListWord){
+    random_device rd;
+    uniform_int_distribution<int> dist(0, sizeListWord-1);
+    int random = dist(rd);
     return random;
 }
 
 string randomWord(){
     vector<string> mots{"avion", "voiture", "lit", "chaise", "ordinateur", "maison"};
-    string word = mots[randomNumber()];
+    const int listSize = mots.size();
+    string word = mots[randomNumber(listSize)];
     return word;
 }
 
@@ -45,6 +34,7 @@ char askLetter(){
     char userLetter;
     cout << "Enter une lettre : \n=> ";
     cin >> userLetter;
+    clear();
     return userLetter;
 }
 
@@ -58,21 +48,17 @@ bool letterInWord(char letter, string word){
     return false;
 }
 
-bool rightWord(const vector<char> letters, string word){
+bool rightWord(vector<char>& letters, string word){
     const int wordSize = word.size();
-    const int letterListSize = letters.size();
-    if(wordSize != letterListSize){
-        return false;
-    }
     for(int i=0; i<wordSize; i++){
-        if(letters[i]!=word[i]){
+        if(letters[i]!= word[i]){
             return false;
         }
     }
     return true;
 }
 
-void display(string word, vector<char> letters){
+void display(string word, vector<char> letters, int tryLeft){
     const int wordSize = word.size();
     for(int i=0; i<wordSize; i++){
         if(word[i]==letters[i]){
@@ -80,7 +66,9 @@ void display(string word, vector<char> letters){
         }else{
             cout << "_ ";
         }
+        
     }
+    cout << "\t\tEssais restant : " << tryLeft << endl;
     print("\n\n");
 }
 
@@ -96,31 +84,40 @@ vector<char> addRigtLetter(char letter, vector<char> a, string word){
 
 int main(){
     print("Bienvenu dans le jeu du pendu !\n\n");
-    int userTry = iInput("Entrer le nombre d'essais que vous souhaitez : ");
+    int userTry = 7;
     string word = randomWord();
     char currentLetter;
-    print(word);
     vector<char> userWord(word.size());
-    bool win = false;
     print("Mot a deviner :\n");
     const int wordSize = word.size();
     for(int a=0; a<wordSize; a++){
         cout << "_ ";
     }
-    print("\n\n\n");
-    while(win == false || userTry != 0){
+    print("\n\n");
+    while(userTry != 0){
         currentLetter = askLetter();
-        if(rightWord(userWord, word)){
-            win = true;
-        }if(letterInWord(currentLetter, word)){
+        if(letterInWord(currentLetter, word)){
             cout << "La lettre " << currentLetter << " est dans le mot !\n" << endl;
             userWord = addRigtLetter(currentLetter, userWord, word);
-            display(word, userWord);
+            if(rightWord(userWord, word)){
+                print("\nFélicitations vous avez trouvé le mot !\n");
+                cout << "Il vous restait " << userTry << " essais";
+                if(userTry>3){
+                    cout << ", pas mal !" << endl;
+                }else{
+                    cout << ", c'était juste !!" << endl;
+                }
+                cin.ignore();
+                break;
+            }
+            display(word, userWord, userTry);
         }else{
             userTry --;
-            print("\nCette lettre n'est pas dans le mot !");
-            display(word, userWord);
-            cout << "Il vous reste maintenant " << userTry << " essais !" << endl;
+            cout << "La lettre " << currentLetter << " n'est pas dans le mot !" << endl;
+            display(word, userWord, userTry);
+            if(userTry == 0){
+                cout << "Vous avez malheureusement perdu, le mot était : " << word << endl;
+            }
         }
     }
     return 0;
