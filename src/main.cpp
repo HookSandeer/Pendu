98a163b18c05ff56e1cd7fcf0579301ef4f0272f
HookSandeer <hooksander@proton.me>
2023-03-14 10:57:05 +0100

    Update Script

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,14 @@ void print(string text){
 
 string sInput(string text){
     string input;
-    cout << text << endl;
+    cout << text;
     getline(cin, input);
     return input;
 }
 
 int iInput(string text){
     int input;
-    print(text);
+    cout << text;
     cin >> input;
     return input;
 }
@@ -30,16 +30,6 @@ void clean(){
     }
 }
 
-void display(int size, int tryLeft){
-    print("Entrer votre lettre en minuscule :");
-    print("Mot a trouver :\n\n");
-    for(int i=0; i<size; i++){
-        cout << "_ ";
-    }
-    print("\n\n\n");
-    cout << "Il vous reste " << tryLeft << " essais." << endl;
-}
-
 int randomNumber(){
     int random = rand()%6;
     return random;
@@ -48,11 +38,12 @@ int randomNumber(){
 string randomWord(){
     vector<string> mots{"avion", "voiture", "lit", "chaise", "ordinateur", "maison"};
     string word = mots[randomNumber()];
+    return word;
 }
 
 char askLetter(){
     char userLetter;
-    cout << "Enter une lettre : \n=> " << endl;
+    cout << "Enter une lettre : \n=> ";
     cin >> userLetter;
     return userLetter;
 }
@@ -68,21 +59,60 @@ bool letterInWord(char letter, string word){
     }
 }
 
+bool rightWord(const vector<char> letters, string word){
+    const int wordSize = word.size();
+    const int letterListSize = letters.size();
+    char currentLetter;
+    if(wordSize != letterListSize){
+        return false;
+    }
+    for(int i=0; i<wordSize; i++){
+        if(letters[i]!=word[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+void display(string word, const vector<char> letters){
+    const int wordSize = word.size();
+    for(int i=0; i<wordSize; i++){
+        if(word[i]==letters[i]){
+            cout << word[i] << " ";
+        }else{
+            cout << "_ ";
+        }
+    }
+}
+
+void addRigtLetter(char letter, vector<char> userWord, string word){
+    const int wordSize = word.size();
+    for(int i=0; i<wordSize; i++){
+        if(letter==word[i]){
+            userWord[i] = letter;
+        }
+    }
+}
+
 int main(){
     print("Bienvenu dans le jeu du pendu !\n\n");
     int userTry = iInput("Entrer le nombre d'essais que vous souhaitez :");
     string word = randomWord();
-    vector<char> findLetter;
+    vector<char> userWord(word.size());
     const int wordSize = word.size();
     bool win = false;
     while(win == false || userTry != 0){
         char currentLetter = askLetter();
-        if(letterInWord(currentLetter, word)){
-            cout << "La lettre " << currentLetter << " est dans le mot !" << endl;
-            findLetter.insert(findLetter.end(), currentLetter);
+        if(rightWord(userWord, word)){
+            win = true;
+        }if(letterInWord(currentLetter, word)){
+            cout << "La lettre " << currentLetter << " est dans le mot !\n" << endl;
+            addRigtLetter(currentLetter, userWord, word);
+            display(word, userWord);
         }else{
             userTry --;
             print("\nCette lettre n'est pas dans le mot !");
+            display(word, userWord);
             cout << "Il vous reste maintenant " << userTry << " essais restant !" << endl;
         }
     }
